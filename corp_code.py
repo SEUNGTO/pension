2ce3e2895da01f8f32c09253ec2102dc747f0789
SEUNGTO <tmdduq9008@gmail.com>
2025-09-15 23:22:05 +0900

    데이터 경로 등 일부 수정함

--- corp_code.py
+++ corp_code.py
@@ -7,6 +7,7 @@ import zipfile
 import xmltodict
 import pandas as pd
 
+# 코드 출처 : https://wikidocs.net/112853
 
 url = "https://opendart.fss.or.kr/api/corpCode.xml"
 api_key = os.getenv('DART_API_KEY')
@@ -23,4 +24,5 @@ xml = zfile.read("CORPCODE.xml").decode("utf-8")
 dict_data = xmltodict.parse(xml)
 data = dict_data['result']['list']
 df = pd.DataFrame(data)
-df.to_csv('corp_code.csv')
+os.makedirs('data', exist_ok=True)
+df.to_csv('data/corp_code.csv', sep = "\t", index = False)
